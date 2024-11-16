4c5cb9e0af5f80310f54510e3c80cf35f4ae52fa
dangelrubio <rubio252010@GMAIL.COM>
2024-11-16 17:29:20 +0100

    feat: add trigger animation

--- js/main.js
+++ js/main.js
@@ -5,9 +5,12 @@ let menuStatus = 1;
 trigger.addEventListener('click', () =>{
     if(menuStatus === 1 ){
         mobileMenu.classList.add('menu-is-open');
+        trigger.classList.add('trigger-is-open');
         menuStatus = 0;
+        
     }else{
         mobileMenu.classList.remove('menu-is-open');
+        trigger.classList.remove('trigger-is-open');
         menuStatus = 1;
     }
     
